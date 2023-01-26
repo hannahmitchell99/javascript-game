a387f52d5357d1b279387454bf5057c147122734
Hannah <hannahamitchell99@gmail.com>
2023-01-26 16:59:01 +0200

    added files and superhero data

---
+++ data/superheroes.js
@@ -0,0 +1,59 @@
+const superheroArray = [
+    {
+        name: "Harley Quinn",
+        health: 2000,
+        sprite: "...",
+        specialAbility: "an explosive up her sleeve",
+        specialAbilityDamage: 300,
+        signatureMove: "hand-to-hand combat",
+        signatureMoveDamage: 150
+    },
+    {
+        name: "Storm",
+        health: 1900,
+        sprite: "...",
+        specialAbility: "here comes a thunderstorm!",
+        specialAbilityDamage: 400,
+        signatureMove: "wind whiplash",
+        signatureMoveDamage: 150
+    },
+    {
+        name: "Wonder Woman",
+        health: 2500,
+        sprite: "...",
+        specialAbility: "whack with the lasso of truth",
+        specialAbilityDamage: 400,
+        signatureMove: "super fighting stamina",
+        signatureMoveDamage: 150
+    },
+    {
+        name: "Spider Man",
+        health: 1900,
+        sprite: "...",
+        specialAbility: "spidey senses are tingling...",
+        specialAbilityDamage: 400,
+        signatureMove: "web right to the face!",
+        signatureMoveDamage: 150
+    },
+    {
+        name: "The Hulk",
+        health: 2500,
+        sprite: "...",
+        specialAbility: "HULK EXTRA ANGRY SMASH!",
+        specialAbilityDamage: 400,
+        signatureMove: "HULK SMASH!",
+        signatureMoveDamage: 200
+    },
+    {
+        name: "Wolverine",
+        health: 3000,
+        sprite: "...",
+        specialAbility: "superhuman speed!",
+        specialAbilityDamage: 400,
+        signatureMove: "here comes a claw!",
+        signatureMoveDamage: 200
+    },
+    
+
+    
+]
