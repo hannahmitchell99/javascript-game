e3b8b9f2c9f227deeaeaa5b138427e934d683d08
Hannah <hannahamitchell99@gmail.com>
2023-01-27 17:56:39 +0200

    worked on select hero button

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 import superHeroArray from "./data/superheroes.js";
 
-// DOM variables 
-const gameContainer = document.querySelector(".game-container")
-const playerCard = document.querySelector(".game-container__player-card")
-const oppCard = document.querySelector(".game-container__opponent-card")
-const gameDisplay = document.querySelector(".game-container__display")
-const newGameButton = document.querySelector("#new-game")
+// DOM variables
+const gameContainer = document.querySelector(".game-container");
+const playerCard = document.querySelector(".game-container__player-card");
+const oppCard = document.querySelector(".game-container__opponent-card");
+const gameDisplay = document.querySelector(".game-container__display");
+const newGameButton = document.querySelector("#new-game");
+const selectPlayerButtons = document.querySelectorAll(".superhero");
 
-
-const harleyQuinn = document.querySelector("#harley-quinn")
-const storm = document.querySelector("#storm")
-const wonderWoman = document.querySelector("#wonder-woman")
-const spiderMan = document.querySelector("#spider-man")
-const theHulk = document.querySelector("#the-hulk")
-const wolverine = document.querySelector("#wolverine")
-
-
+// const harleyQuinn = document.querySelector("#harley-quinn")
+// const storm = document.querySelector("#storm")
+// const wonderWoman = document.querySelector("#wonder-woman")
+// const spiderMan = document.querySelector("#spider-man")
+// const theHulk = document.querySelector("#the-hulk")
+// const wolverine = document.querySelector("#wolverine")
 
 // Functions
 
+let userHero = ""
+const selectSuperhero = (event) => {
+  for (let index = 0; index < superHeroArray.length; index++) {
+    if (event.target.innerHTML === superHeroArray[index].name) {
+      console.log(superHeroArray[index].name)
+      return userHero = superHeroArray[index].name
+    }
+  }
+};
 
 
-// User selects player function
+// Events
 
+selectPlayerButtons.forEach((selectPlayerButton) => {
+  selectPlayerButton.addEventListener("click", selectSuperhero);
+});
 
+console.log(userHero)
 
+//console.log(superHeroArray[0].signatureMove)
