ce46e42294c43a32f5f04ffb096e3ebf7e971e4a
Hannah <hannahamitchell99@gmail.com>
2023-01-27 16:26:15 +0200

    removed some styling, added drop down box for player choice

---
+++ script.js
@@ -0,0 +1,9 @@
+import superheroArray from "./data/superheroes";
+
+// DOM variables 
+const gameContainer = document.querySelector(".game-container")
+const playerCard = document.querySelector(".game-container__player-card")
+const oppCard = document.querySelector(".game-container__opponent-card")
+const gameDisplay = document.querySelector(".game-container__display")
+const newGameButton = document.querySelector("#new-game")
+
