ab7721dd40760b3c185658fdab2cc4f69dd56cd1
Hannah <hannahamitchell99@gmail.com>
2023-01-31 17:57:44 +0200

    added html population and random opponent functions

--- script.js
+++ script.js
@@ -7,33 +7,80 @@ const oppCard = document.querySelector(".game-container__opponent-card");
 const gameDisplay = document.querySelector(".game-container__display");
 const newGameButton = document.querySelector("#new-game");
 const selectPlayerButtons = document.querySelectorAll(".superhero");
+const playButton = document.querySelector(".play");
 
-// const harleyQuinn = document.querySelector("#harley-quinn")
-// const storm = document.querySelector("#storm")
-// const wonderWoman = document.querySelector("#wonder-woman")
-// const spiderMan = document.querySelector("#spider-man")
-// const theHulk = document.querySelector("#the-hulk")
-// const wolverine = document.querySelector("#wolverine")
+let userHero = "hello";
+let computerHero = "hi";
 
 // Functions
 
-let userHero = ""
+// user selects their superhero
 const selectSuperhero = (event) => {
   for (let index = 0; index < superHeroArray.length; index++) {
     if (event.target.innerHTML === superHeroArray[index].name) {
-      console.log(superHeroArray[index].name)
-      return userHero = superHeroArray[index].name
+      console.log(superHeroArray[index]);
+      userHero = (superHeroArray[index])
+      return userHero;
+      
     }
   }
 };
 
+// computer selected superhero 
 
-// Events
+const selectOpponent = () =>{
+  const random = Math.floor(Math.random() * superHeroArray.length)
+  console.log(superHeroArray[random])
+  computerHero = superHeroArray[random]
+  return computerHero
+  
+  
+  //return computerHero
+}
+
+
+// display selected superhero
+
+const displaySelectedSuperhero = (superHeroObject) => {
+  const playerCardHTML = 
+  `<div class="player-heading">
+       <h3 class="player-heading__name">${superHeroObject.name}</h3>
+   </div>
+   <img class="player-sprite" src="${superHeroObject.sprite}" alt="${superHeroObject.name} sprite">
+   <div class="player-abilities">
+       <h4>Health:${superHeroObject.health}</h4>
+       <h4>Special Ability:${superHeroObject.specialAbility}</h4>
+       <h4>Signature Move:${superHeroObject.signatureMove}</h4>
+       <button id="special-ability-button">Special Ability!</button>
+       <button id="signature-move-button">Signature Move!</button>
+   </div>
+`;
+  return playerCard.innerHTML = playerCardHTML;
+};
+
+
+const displayOpponent = (superHeroObject) =>{
+  const oppCardHTML = 
+  `<div class="opponent-heading">
+       <h3 class="opponent-heading__name">${superHeroObject.name}</h3>
+   </div>
+   <img class="opponent-sprite" src="${superHeroObject.sprite}" alt="${superHeroObject.name} sprite">
+   <div class="opponent-abilities">
+       <h4>Health:${superHeroObject.health}</h4>
+       <h4>Special Ability:${superHeroObject.specialAbility}</h4>
+       <h4>Signature Move:${superHeroObject.signatureMove}</h4>
+   </div>
+`;
+  return oppCard.innerHTML = oppCardHTML;
+}
+
+// // Events
 
 selectPlayerButtons.forEach((selectPlayerButton) => {
   selectPlayerButton.addEventListener("click", selectSuperhero);
+  selectPlayerButton.addEventListener("click", selectOpponent);
 });
 
-console.log(userHero)
 
-//console.log(superHeroArray[0].signatureMove)
+playButton.addEventListener("click", () => displaySelectedSuperhero(userHero))
+playButton.addEventListener("click", () => displayOpponent(computerHero))
