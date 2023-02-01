4a5510b873bf17d88aab3df92290823b3da1dd80
Hannah <hannahamitchell99@gmail.com>
2023-02-01 12:45:42 +0200

    fixed display functionality, started on health function

--- script.js
+++ script.js
@@ -10,9 +10,14 @@ const selectPlayerButtons = document.querySelectorAll(".superhero");
 const playButton = document.querySelector(".play");
 const specialAbilityButton = document.querySelector("#special-ability-button");
 const signatureMoveButton = document.querySelector("#signature-move-button");
+const healthContainer = document.querySelector(".game-container__health");
 
 let userHero = "hello";
 let computerHero = "hi";
+let playerMove = "playermove";
+let computerMove = "computer move";
+let currentPlayerHealth = 0;
+
 
 // Functions
 
@@ -44,14 +49,14 @@ const selectOpponent = () => {
 
 const playerSelectMove = (event) =>{
   if(event.target.innerHTML === "Special Ability!"){
-    let playerMove = userHero.specialAbilityDamage
+    playerMove = userHero.specialAbility
     console.log(playerMove)
-    return playerMove
+    return playerMove;
     
   }else if(event.target.innerHTML === "Signature Move!"){
-    let playerMove = userHero.signatureMoveDamage
+    playerMove = userHero.signatureMove
     console.log(playerMove)
-    return playerMove
+    return playerMove;
   }
  
 }
@@ -60,15 +65,32 @@ const playerSelectMove = (event) =>{
 const computerSelectMove = () =>{
   const random = Math.floor(Math.random()*2)
   if(random ===0){
-    let computerMove = computerHero.specialAbilityDamage
+    computerMove = computerHero.specialAbility
     console.log(computerMove)
     return computerMove
   } else{
-    let computerMove = computerHero.signatureMoveDamage
+    computerMove = computerHero.signatureMove
     console.log(computerMove)
     return computerMove
   }
 }
+
+// game functions
+
+const calculatePlayerHealth = () =>{
+  if (computerMove === computerHero.specialAbility){
+    let computerDamage = computerHero.specialAbilityDamage;
+    currentPlayerHealth -= computerDamage
+    console.log(currentPlayerHealth)
+    return currentPlayerHealth
+  } else if (computerMove === computerHero.signatureMove) {
+    let computerDamage = computerHero.signatureMoveDamage
+    currentPlayerHealth -= computerDamage
+    console.log(currentPlayerHealth)
+    return currentPlayerHealth
+  }
+
+ }
 
 //Display Functions
 
@@ -80,12 +102,12 @@ const displaySelectedSuperhero = (superHeroObject) => {
    </div>
    <img class="player-sprite" src="${superHeroObject.sprite}" alt="${superHeroObject.name} sprite">
    <div class="player-abilities">
-       <h4>Health:${superHeroObject.health}</h4>
+       <h4>Original Health:${superHeroObject.health}</h4>
        <h4>Special Ability:${superHeroObject.specialAbility}</h4>
        <h4>Signature Move:${superHeroObject.signatureMove}</h4>
    </div>
 `;
-  return (playerCard.innerHTML = playerCardHTML);
+playerCard.innerHTML = playerCardHTML;
 };
 
 // display opponent
@@ -96,22 +118,47 @@ const displayOpponent = (superHeroObject) => {
    </div>
    <img class="opponent-sprite" src="${superHeroObject.sprite}" alt="${superHeroObject.name} sprite">
    <div class="opponent-abilities">
-       <h4>Health:${superHeroObject.health}</h4>
+       <h4>Original Health:${superHeroObject.health}</h4>
        <h4>Special Ability:${superHeroObject.specialAbility}</h4>
        <h4>Signature Move:${superHeroObject.signatureMove}</h4>
    </div>
 `;
-  return (oppCard.innerHTML = oppCardHTML);
+oppCard.innerHTML = oppCardHTML;
 };
 
-// open display
-const displayMoveChoice = () => {
-  const displayHTML = `<h2>You Chose:</h2>
- <h2>Opponent Chose: </h2>
+// display move choice
+
+const displayMoveChoice = (computerMove, playerMove, currentPlayerHealth) => {
+  const displayHTML = `<h2>You Chose: ${playerMove} </h2>
+ <h2>Opponent Chose: ${computerMove} </h2>
  <h2> You took x damage!</h2>
- <h2> Opponent took x damage!</h2>`;
-  return (gameDisplay.innerHTML = displayHTML);
+ <h2> Your health is ${currentPlayerHealth}</h2>
+ <h2> Opponent took x damage!</h2>
+ <h2>Their health is </h2>`;
+ gameDisplay.innerHTML = displayHTML
+
 };
+
+
+
+// const displayPlayerChoice = () => {
+
+//   if (displayPlayerC
+//   const displayHTML = `<h2>You Chose: ${playerMove} </h2>
+//   <h2> Opponent took x damage!</h2>`
+//   return gameDisplay.innerHTML =displayOpponent
+  
+// }
+
+// display player health
+
+const displayPlayerHealth=()=>{
+  const displayHTML = `
+ <h2> You took x damage!</h2>
+ <h2> Your health is ${currentPlayerHealth}</h2>`
+
+ return healthContainer.innerHTML = displayHTML
+}
 
 
 // Events
@@ -126,6 +173,12 @@ playButton.addEventListener("click", () => displayOpponent(computerHero));
 
 specialAbilityButton.addEventListener("click", playerSelectMove)
 specialAbilityButton.addEventListener("click", computerSelectMove)
+specialAbilityButton.addEventListener("click", calculatePlayerHealth)
 
 signatureMoveButton.addEventListener("click", playerSelectMove)
 signatureMoveButton.addEventListener("click", computerSelectMove)
+signatureMoveButton.addEventListener("click", calculatePlayerHealth)
+
+
+signatureMoveButton.addEventListener("click", () => displayMoveChoice(computerMove, playerMove, currentPlayerHealth))
+specialAbilityButton.addEventListener("click", () => displayMoveChoice(computerMove, playerMove, currentPlayerHealth))
